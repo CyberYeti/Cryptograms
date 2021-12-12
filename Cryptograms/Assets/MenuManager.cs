fef7eda173c582a722aaa1c7ec0ced8371cc0b6a
CyberYeti <yitmengchin@gmail.com>
2021-12-12 00:12:30 -0800

    Options Dropdown UI
    +Made options dropdown switch between panels
    +Fixed UI scaling problem

--- Cryptograms/Assets/MenuManager.cs
+++ Cryptograms/Assets/MenuManager.cs
@@ -8,12 +8,27 @@ public class MenuManager : MonoBehaviour
     [SerializeField] List<string> optionsNames;
     [SerializeField] GameObject[] optionsPanels;
     [SerializeField] Dropdown optionsDropdown;
-    // Start is called before the first frame update
+
+    int currentIndex = 0;
+
     void Start()
     {
         optionsDropdown.ClearOptions();
         optionsDropdown.AddOptions(optionsNames);
+        optionsDropdown.value = 0;
+        optionsDropdown.RefreshShownValue();
+
+        foreach (GameObject panel in optionsPanels)
+        {
+            panel.SetActive(false);
+        }
+        optionsPanels[currentIndex].SetActive(true);
     }
 
-    
+    public void OptionsDropdown(int _index)
+    {
+        optionsPanels[currentIndex].SetActive(false);
+        optionsPanels[_index].SetActive(true);
+        currentIndex = _index;
+    }
 }
